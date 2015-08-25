6dae2f2c6c8e5c4e0164d1c88d8a12f0fa90fdf1
Corrie Smith & Josue Valverde <student@epicodus.com>
2015-08-25 12:17:09 -0700

    add custom styling; add display for questions and update question counter; add display for surveys and number of questions.

--- app.rb
+++ app.rb
@@ -4,12 +4,41 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/survey')
-require('.lib/question')
+require('./lib/question')
 require('pg')
 require('pry')
 
 get('/') do
-  @question = Question.all()
-  @survey = Survey.all()
   erb(:index)
+end
+
+get('/surveys') do
+  @surveys = Survey.all()
+  erb(:survey)
+end
+
+post('/surveys') do
+  name = params.fetch("name")
+  survey = Survey.create({:name => name, :num_questions => 0})
+  @surveys = Survey.all()
+  erb(:survey)
+end
+
+get('/surveys/:id') do
+  @id = params['id'].to_i
+  @surveys = Survey.find(params.fetch('id').to_i)
+  @questions = @surveys.questions()
+  erb(:question)
+end
+
+post('/surveys/:id') do
+  @id = params['id'].to_i
+  question = params['question']
+  Question.create({:question => question, :survey_id => @id})
+  survey = Survey.find(@id)
+  num_questions = survey.num_questions + 1
+  survey.update({:num_questions => num_questions})
+  @surveys = Survey.find(@id.to_i)
+  @questions = survey.questions()
+  erb(:question)
 end
